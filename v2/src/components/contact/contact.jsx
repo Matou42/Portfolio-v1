7da1725f11f9a55d0a9a79daf57595be4f87ca48
Matou <tissot.maxence2812@gmail.com>
2024-06-02 21:33:53 +0200

    contact

--- v2/src/components/contact/contact.jsx
+++ v2/src/components/contact/contact.jsx
@@ -1,28 +1,45 @@
-// Contact.jsx
-
 import { useState } from 'react';
 import './contact.css';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
-  // États pour les champs du formulaire, les messages d'erreur et l'état de soumission
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
-  const [errors, setErrors] = useState({}); // Objet contenant les messages d'erreur pour chaque champ
+  const [errors, setErrors] = useState({});
   const [submitted, setSubmitted] = useState(false);
+  const [emailError, setEmailError] = useState(null);
 
-  // Fonction de validation du formulaire
+  const sendEmail = () => {
+    console.log('Envoi de l\'email en cours...');
+    console.log('Nom:', name);
+    console.log('Email:', email);
+    console.log('Message:', message);
+
+    emailjs.send("service_vdxlaz7", "template_j3v69mg", {
+      reply_to: email,
+      message: message
+    }, 'IbLRh68IWib2ejfnM')
+    .then((result) => {
+      console.log('Email envoyé avec succès !', result.text);
+      setSubmitted(true);
+      setEmailError(null);
+    })
+    .catch((error) => {
+      console.error('Erreur lors de l\'envoi de l\'email :', error.text);
+      setEmailError("Une erreur s'est produite lors de l'envoi de votre mail. Veuillez réessayer plus tard. Si le problème persiste, veuillez me contacter à l'adresse suivante : <a href=\"mailto:contact.maxence2812@gmail.com\">contact.maxence2812@gmail.com</a>.");
+    });
+  };
+
   const validateForm = () => {
     let errors = {};
     let isValid = true;
 
-    // Validation du champ Nom
     if (!name.trim()) {
       errors.name = "Le champ Nom est requis";
       isValid = false;
     }
 
-    // Validation du champ Email
     if (!email.trim()) {
       errors.email = "Le champ Email est requis";
       isValid = false;
@@ -31,67 +48,60 @@ const Contact = () => {
       isValid = false;
     }
 
-    // Validation du champ Message
     if (!message.trim()) {
       errors.message = "Le champ Message est requis";
       isValid = false;
     }
 
-    // Mise à jour de l'état des erreurs
     setErrors(errors);
 
     return isValid;
-  }
+  };
 
-  // Fonction de gestion de la soumission du formulaire
   const handleSubmit = (e) => {
-    e.preventDefault(); // Empêche le rechargement de la page lors de la soumission du formulaire
+    e.preventDefault();
 
-    // Validation du formulaire
     if (validateForm()) {
-      // Vous pouvez implémenter ici la logique d'envoi du formulaire, par exemple avec une requête HTTP
-
-      // Affichage des valeurs dans la console (pour le débogage)
+      console.log('Formulaire valide, envoi de l\'email...');
+      sendEmail();
       console.log('Nom:', name);
       console.log('Email:', email);
       console.log('Message:', message);
-
-      // Mise à jour de l'état de soumission pour afficher le message de confirmation
-      setSubmitted(true);
     }
-  }
+  };
 
   return (
     <section id="contact">
       <h2>Contact</h2>
       {submitted ? (
-        <p>Merci de nous avoir contactés ! Nous vous répondrons dès que possible.</p>
+        <div className="message-container success">
+          <h3>Contact</h3>
+          <p>Merci de nous avoir contactés ! Nous vous répondrons dès que possible.</p>
+        </div>
       ) : (
-        <form onSubmit={handleSubmit}>
-          {/* Champ de saisie du nom */}
+        <form id="contact-form" onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor="name">Nom :</label>
-            <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} required />
-            {errors.name && <span className="error">{errors.name}</span>} {/* Affichage du message d'erreur */}
+            <textarea type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="Votre nom" required></textarea>
+            {errors.name && <span className="error">{errors.name}</span>}
           </div>
-          {/* Champ de saisie de l'email */}
           <div className="form-group">
             <label htmlFor="email">Email :</label>
-            <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-            {errors.email && <span className="error">{errors.email}</span>} {/* Affichage du message d'erreur */}
+            <textarea type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Votre mail" required></textarea>
+            {errors.email && <span className="error">{errors.email}</span>}
           </div>
-          {/* Champ de saisie du message */}
           <div className="form-group">
-            <label htmlFor="message">Message :</label>
-            <textarea id="message" value={message} onChange={(e) => setMessage(e.target.value)} required></textarea>
-            {errors.message && <span className="error">{errors.message}</span>} {/* Affichage du message d'erreur */}
+            <textarea name="message" value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Votre message" required></textarea>
+            {errors.message && <span className="error">{errors.message}</span>}
           </div>
-          {/* Bouton de soumission du formulaire */}
           <button type="submit">Envoyer</button>
+          {emailError && (
+            <div className="message-container error" dangerouslySetInnerHTML={{ __html: emailError }}></div>
+          )}
         </form>
       )}
     </section>
   );
-}
+};
 
 export default Contact;
